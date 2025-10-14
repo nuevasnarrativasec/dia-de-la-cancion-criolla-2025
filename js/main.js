5d76707b9bd5b5973b103c3407dc1b6834e1c476
nuevasnarrativasec <nuevasnarrativasec@gmail.com>
2025-10-14 11:17:01 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,475 @@
+$(document).ready(function() {
+
+
+    // cabecera
+    $('#my-movil').click(function() {
+        $('#bloque-sabina').fadeIn('slow');
+        $('#my-movil').fadeOut('slow');
+    });
+
+
+
+    // joven y aventurer  
+    $('.boton-1').click(function() {
+        $('#tema-1').fadeIn('slow');
+        $('#bloque-sabina-desktop').fadeOut('slow');
+        $('#bloque-sabina').fadeOut('slow');
+        $('#my-movil').fadeOut('slow');
+    });
+    // boxeador
+    $('.boton-2').click(function() {
+        $('#tema-2').fadeIn('slow');
+        $('#bloque-sabina-desktop').fadeOut('slow');
+        $('#bloque-sabina').fadeOut('slow');
+        $('#my-movil').fadeOut('slow');
+    });
+    // pirata
+    $('.boton-3').click(function() {
+        $('#tema-3').fadeIn('slow');
+        $('#bloque-sabina-desktop').fadeOut('slow');
+        $('#bloque-sabina').fadeOut('slow');
+        $('#my-movil').fadeOut('slow');
+    });
+    // intelectual
+    $('.boton-4').click(function() {
+        $('#tema-4').fadeIn('slow');
+        $('#bloque-sabina-desktop').fadeOut('slow');
+        $('#bloque-sabina').fadeOut('slow');
+        $('#my-movil').fadeOut('slow');
+    });
+    // viejo
+    $('.boton-5').click(function() {
+        $('#tema-5').fadeIn('slow');
+        $('#bloque-sabina-desktop').fadeOut('slow');
+        $('#bloque-sabina').fadeOut('slow');
+        $('#my-movil').fadeOut('slow');
+    });
+    // guapo
+    $('.boton-6').click(function() {
+        $('#tema-6').fadeIn('slow');
+        $('#bloque-sabina-desktop').fadeOut('slow');
+        $('#bloque-sabina').fadeOut('slow');
+        $('#my-movil').fadeOut('slow');
+    });
+    // poeta
+    $('.boton-7').click(function() {
+        $('#tema-7').fadeIn('slow');
+        $('#bloque-sabina-desktop').fadeOut('slow');
+        $('#bloque-sabina').fadeOut('slow');
+        $('#my-movil').fadeOut('slow');
+    });
+
+
+    // joven tema 1
+    $('.tema-a-1').click(function() {
+        $('#tema-1').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-a1').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // joven tema 2
+    $('.tema-a-2').click(function() {
+        $('#tema-1').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#creditos').fadeIn('slow');
+        $('#frase-a2').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // joven tema 3
+    $('.tema-a-3').click(function() {
+        $('#tema-1').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-a3').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // joven tema 4
+    $('.tema-a-4').click(function() {
+        $('#tema-1').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-a4').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+
+    });
+    // joven tema 5
+    $('.tema-a-5').click(function() {
+        $('#tema-1').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-a5').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+
+    });
+    // joven tema 6
+    $('.tema-a-6').click(function() {
+        $('#tema-1').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-a6').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+
+    });
+    // joven tema 7
+    $('.tema-a-7').click(function() {
+        $('#tema-1').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-a7').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+
+    });
+    // joven tema 8
+    $('.tema-a-8').click(function() {
+        $('#tema-1').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-a8').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+
+    });
+
+    // boxeador tema 1
+    $('.tema-b-1').click(function() {
+        $('#tema-2').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-b1').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+
+    });
+    // boxeador tema 2
+    $('.tema-b-2').click(function() {
+        $('#tema-2').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-b2').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+
+    });
+    // boxeador tema 3
+    $('.tema-b-3').click(function() {
+        $('#tema-2').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-b3').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // boxeador tema 4
+    $('.tema-b-4').click(function() {
+        $('#tema-2').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-b4').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+
+    });
+    // boxeador tema 5
+    $('.tema-b-5').click(function() {
+        $('#tema-2').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-b5').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // boxeador tema 6
+    $('.tema-b-6').click(function() {
+        $('#tema-2').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-b6').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // boxeador tema 7
+    $('.tema-b-7').click(function() {
+        $('#tema-2').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-b7').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // boxeador tema 8
+    $('.tema-b-8').click(function() {
+        $('#tema-2').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-b8').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+
+    // pirata tema 1
+    $('.tema-c-1').click(function() {
+        $('#tema-3').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-c1').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // pirata tema 2
+    $('.tema-c-2').click(function() {
+        $('#tema-3').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-c2').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // pirata tema 3
+    $('.tema-c-3').click(function() {
+        $('#tema-3').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-c3').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // pirata tema 4
+    $('.tema-c-4').click(function() {
+        $('#tema-3').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-c4').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // pirata tema 5
+    $('.tema-c-5').click(function() {
+        $('#tema-3').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-c5').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // pirata tema 6
+    $('.tema-c-6').click(function() {
+        $('#tema-3').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-c6').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // pirata tema 7
+    $('.tema-c-7').click(function() {
+        $('#tema-3').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-c7').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // pirata tema 8
+    $('.tema-c-8').click(function() {
+        $('#tema-3').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-c8').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+
+    // intelectual tema 1
+    $('.tema-d-1').click(function() {
+        $('#tema-4').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-d1').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // intelectual tema 2
+    $('.tema-d-2').click(function() {
+        $('#tema-4').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-d2').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // intelectual tema 3
+    $('.tema-d-3').click(function() {
+        $('#tema-4').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-d3').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // intelectual tema 4
+    $('.tema-d-4').click(function() {
+        $('#tema-4').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-d4').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // intelectual tema 5
+    $('.tema-d-5').click(function() {
+        $('#tema-4').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-d5').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // intelectual tema 6
+    $('.tema-d-6').click(function() {
+        $('#tema-4').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-d6').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // intelectual tema 7
+    $('.tema-d-7').click(function() {
+        $('#tema-4').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-d7').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // intelectual tema 8
+    $('.tema-d-8').click(function() {
+        $('#tema-4').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-d8').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+
+    // viejo tema 1
+    $('.tema-e-1').click(function() {
+        $('#tema-5').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-e1').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // viejo tema 2
+    $('.tema-e-2').click(function() {
+        $('#tema-5').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-e2').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // viejo tema 3
+    $('.tema-e-3').click(function() {
+        $('#tema-5').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-e3').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // viejo tema 4
+    $('.tema-e-4').click(function() {
+        $('#tema-5').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-e4').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // viejo tema 5
+    $('.tema-e-5').click(function() {
+        $('#tema-5').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-e5').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // viejo tema 6
+    $('.tema-e-6').click(function() {
+        $('#tema-5').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-e6').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // viejo tema 7
+    $('.tema-e-7').click(function() {
+        $('#tema-5').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-e7').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // viejo tema 8
+    $('.tema-e-8').click(function() {
+        $('#tema-5').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-e8').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+
+    // guapo tema 1
+    $('.tema-f-1').click(function() {
+        $('#tema-6').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-f1').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // guapo tema 2
+    $('.tema-f-2').click(function() {
+        $('#tema-6').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-f2').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // guapo tema 3
+    $('.tema-f-3').click(function() {
+        $('#tema-6').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-f3').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // guapo tema 4
+    $('.tema-f-4').click(function() {
+        $('#tema-6').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-f4').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // guapo tema 5
+    $('.tema-f-5').click(function() {
+        $('#tema-6').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-f5').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // guapo tema 6
+    $('.tema-f-6').click(function() {
+        $('#tema-6').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-f6').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // guapo tema 7
+    $('.tema-f-7').click(function() {
+        $('#tema-6').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-f7').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // guapo tema 8
+    $('.tema-f-8').click(function() {
+        $('#tema-6').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-f8').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+
+    // poeta tema 1
+    $('.tema-g-1').click(function() {
+        $('#tema-7').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-g1').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // poeta tema 2
+    $('.tema-g-2').click(function() {
+        $('#tema-7').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-g2').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // poeta tema 3
+    $('.tema-g-3').click(function() {
+        $('#tema-7').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-g3').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // poeta tema 4
+    $('.tema-g-4').click(function() {
+        $('#tema-7').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-g4').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // poeta tema 5
+    $('.tema-g-5').click(function() {
+        $('#tema-7').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-g5').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // poeta tema 6
+    $('.tema-g-6').click(function() {
+        $('#tema-7').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-g6').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // poeta tema 7
+    $('.tema-g-7').click(function() {
+        $('#tema-7').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-g7').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+    // poeta tema 8
+    $('.tema-g-8').click(function() {
+        $('#tema-7').fadeOut('slow');
+        $('#my-header').fadeOut('slow');
+        $('#frase-g8').fadeIn('slow');
+        $('#creditos').fadeIn('slow');
+    });
+
+
+
+
+
+});
